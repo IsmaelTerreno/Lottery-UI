838831d440f6ec5a280ceb1df7befc05ee84f942
Carlos Ismael Terreno <carlosismaelterreno@hotmail.com>
2021-02-24 17:20:45 -0300

    Load winner info. Metamask detect account change.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Web3 from 'web3';
 import contract_abi from './build/contracts/Lottery.json';
-const contractAddress = '0x0e7799CEAE9C1640EB5943C10A96fBc69910D922';
+const contractAddress = '0x5612b6D5A9EF2C2B56f00e2349601A8811B0D652';
 const ENTER_PRICE_LOTTERY_IN_ETHER = '0.2';
 let LotteryContract;
 let web3;
 let accounts;
 
-// Modern DApp Browsers
-if (window.ethereum) {
-  web3 = new Web3(window.ethereum);
-  try { 
-     window.ethereum.enable().then(async() =>{
-        accounts = await web3.eth.getAccounts();
-        // User has allowed account access to DApp...
-        LotteryContract = new web3.eth.Contract(contract_abi.abi, contractAddress);
-     });
-  } catch(e) {
-     // User has denied account access to DApp...
+const loadDapp = async () => {
+  // Modern DApp Browsers
+  if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
+    try { 
+      window.ethereum.enable().then(async() =>{
+          accounts = await web3.eth.getAccounts();
+          // User has allowed account access to DApp...
+          LotteryContract = new web3.eth.Contract(contract_abi.abi, contractAddress);
+      });
+      window.ethereum.on('accountsChanged', (chainId) => {
+        // Handle the new chain.
+        // Correctly handling chain changes can be complicated.
+        // We recommend reloading the page unless you have good reason not to.
+        window.location.reload();
+      });
+    } catch(e) {
+      // User has denied account access to DApp...
+    }
+  }
+  // Legacy DApp Browsers
+  else if (window.web3) {
+    web3 = new Web3(window.web3.currentProvider);
+  }
+  // Non-DApp Browsers
+  else {
+    alert('You have to install MetaMask !');
   }
 }
-// Legacy DApp Browsers
-else if (window.web3) {
-   web3 = new Web3(window.web3.currentProvider);
-}
-// Non-DApp Browsers
-else {
-   alert('You have to install MetaMask !');
-}
 
+loadDapp();
 
 const startLottery = async () => {
   const result1 = await LotteryContract.methods.start_new_lottery().send({ from: accounts[0]});
@@ -53,26 +63,70 @@ const getLastWinner = async () => {
   const result4 = await LotteryContract.methods.getLastWinner().call({ from: accounts[0] });
   console.log('Get last winner');
   console.log(result4);
+  return {
+    block: result4[0],
+    address: result4[1],
+    amount: web3.utils.fromWei(result4[2], "ether")
+  };
 };
 const getBalancePrice = async () => {
   const result4 = await LotteryContract.methods.getMainBalance().call({ from: accounts[0] });
   console.log('Get balance price');
   console.log(web3.utils.fromWei(result4, "ether"));
+  return web3.utils.fromWei(result4, "ether");
 };
-const App = () => {
 
+
+
+const App = () => {
+  const [lotteryInfo, setLotteryInfo] = useState({
+    balance:0,
+    winnerInfo:{
+      block: null,
+      address: '',
+      amount: 0,
+      players:null,
+    },
+  });
+  useEffect(() => {
+      setTimeout(() => {
+        const fetchData = async () => {
+          let currentBalance = await getBalancePrice();
+          let winnerInfo = await getLastWinner();
+
+          setLotteryInfo({
+            ...lotteryInfo,
+            balance: currentBalance,
+            winnerInfo,
+          }); 
+        }
+        fetchData();
+     }, 1000);
+  }, []);
+  const { balance, winnerInfo} = lotteryInfo;
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Lottery app base 2.
+          Lottery balance {balance} ETH.
+        </p>
+        <p>
+          Last winner infomation
+        </p>
+        <p>
+          Address {winnerInfo.address}
+        </p>
+        <p>
+          Block: {winnerInfo.block}
+        </p>
+        <p>
+          Amount: {winnerInfo.amount} ETH
         </p>
         <button onClick={startLottery}>Start lottery</button>
         <button onClick={enterIntoLottery}>Participate</button>
         <button onClick={pickWinner}>Pick winner</button>
         <button onClick={getLastWinner}>Get last Winner</button>
-        <button onClick={getBalancePrice}>Get balance price</button>
-        
+        <button onClick={getBalancePrice}>Get balance price</button> 
       </header>
     </div>
   );
