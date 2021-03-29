1a73b75de0bee1efe14c2b262beb11ae47da6cf6
Carlos Ismael Terreno <carlosismaelterreno@hotmail.com>
2021-03-29 12:24:58 -0300

    Update contrac address through .env var

--- src/config.js
+++ src/config.js
@@ -1,16 +1,12 @@
 export const MAIN_APP_NAME = 'Crypto Lottery';
-export const BASE_END_POINT_JOB= process.env.REACT_APP_BASE_END_POINT_JOB;
-export const EMPTY_JWT_ERROR_MESSAGE='There is no JWT access token to make calls to the APIs';
 export const STATUS_401_ERROR_MESSAGE='Calls to the API return 401 Unauthorized';
 export const STATUS_400_ERROR_MESSAGE='Calls to the API return 400 Bad Request';
 export const STATUS_403_ERROR_MESSAGE='Calls to the API return 403 Forbidden';
 export const STATUS_500_ERROR_MESSAGE='Calls to the API return 500 Internal Server Error';
 export const STATUS_504_ERROR_MESSAGE='Calls to the API return 504 Gateway Timeout';
-export const NAMESPACE_ID_JWT_AUTH_CONFIG = process.env.REACT_APP_NAMESPACE_ID_JWT_AUTH_CONFIG
 export const APP_PATH_ROOT = '/';
-export const APP_PATH_DASHBOARD = '/dashboard';
-export const JOB_END_POINT='/api/v1/job';
 export const APP_API_CALL_FAIL = 'APP_API_CALL_FAIL';
-export const CONTRACT_ADDRESS = '0xcDaEDe61c63C840e0a4e78c13f23486852882210';
+export const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
 export const FORMAT_DATE_TIME = "DD-MM-YYYY";
+
 
